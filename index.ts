c52890e37605856de98f2d8afb0f9671752ee794
Thiago Allisson <thiago.allisson@ufpi.edu.br>
2024-11-12 18:39:49 -0300

    Fix cors

--- index.ts
+++ index.ts
@@ -10,6 +10,9 @@ app.use(cors());
 
 app.use((req: Request, res: Response, next: NextFunction) => {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  app.use(cors());
   next();
 });
 
